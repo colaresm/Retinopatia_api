8ed347a1027a3b91af444c1f947f8acc8b54d0fe
colaresm <colaresmarcelo2018@gmail.com>
2022-01-20 09:22:28 -0300

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from redis import Redis
+#from redis import Redis
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse
 import joblib
@@ -10,12 +10,14 @@ from keras.preprocessing import image
 from keras.applications.vgg19 import VGG19
 from keras.applications.vgg19 import preprocess_input
 #import cv2
+import zipfile
 import json
 from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
 import base64
-
+import urllib.request
+import zipfile
 
 
 APP = Flask(__name__)
@@ -26,17 +28,17 @@ CORS(app)
 @app.route('/ret', methods = ['GET'])
 def returnascii():
         inputchr = str(request.args['nome'])
+        type_prediction=str(request.args['tipopredicao'])
         stringfinal=inputchr.replace(" ","+")
         stringfinal=stringfinal.replace("data:image/png;base64,","")
         strinfinal2=str.encode(stringfinal)
         data={}
         data['img'] = strinfinal2
         im = Image.open(BytesIO(base64.b64decode(data['img'])))
-        image_result = open('deer_decode.png', 'wb') # create a writable image and write the decoding result
+        image_result = open('deer_decode2.png', 'wb') # create a writable image and write the decoding result
         image_result.write(base64.b64decode(data['img']))
-      #  print(type(inputchr))
         model = VGG19(weights='imagenet', include_top=False)
-        img = image.load_img('deer_decode.png', target_size=(224, 224))
+        img = image.load_img('deer_decode2.png', target_size=(224, 224))
         img_data = image.img_to_array(img)
         img_data = np.expand_dims(img_data, axis=0)
         img_data = preprocess_input(img_data)
@@ -44,22 +46,44 @@ def returnascii():
         vgg19_feature = model.predict(img_data)
         vgg19_feature_np = np.array(vgg19_feature)
         vgg19_feature_listteste.append(vgg19_feature_np.flatten())
-        crc =joblib.load('SVM_multiclasse.mdl')
-        pred= crc.predict(vgg19_feature_listteste)
-      
-        print(pred)
+
+        #validation of image
+        model_validation_zip = zipfile.ZipFile('models/SVM_validador.zip') 
+        #load binary model
+        model_binary_zip = zipfile.ZipFile('models/SVM_binario.zip') 
+        #load multilcass model
+        model_multiclass_zip = zipfile.ZipFile('models/SVM_multiclasse.zip') 
+
+        clf_validation =joblib.load(model_validation_zip.open('SVM_validador.mdl'))
+        clf_binary =joblib.load(model_binary_zip.open('SVM_binario.mdl'))
+        clf_multiclass =joblib.load(model_multiclass_zip.open('SVM_multiclasse.mdl'))
+
         result = ""
-        
-        if( pred[0]=="leve"):
-                result="Leve"
-        if( pred[0]=="moderado"):
-                result="Moderado"
-        if( pred[0]=="grave"):
-                result="Grave"
+        if(clf_validation.predict(vgg19_feature_listteste)=="olho"):
+                if(type_prediction=='binario'):
+                        pred= clf_binary.predict(vgg19_feature_listteste)
+                        if( pred[0]=="absence"):
+                                result="Ausência"
+                        else:
+                                result="Presença"
+                               
+                if(type_prediction=='multiclasse'):
+                        pred= clf_multiclass.predict(vgg19_feature_listteste)
+                        if( pred[0]=="absence"):
+                                result="Ausência"
+                              
+                        if( pred[0]=="leve"):
+                                result="Leve"
+                                
+                        if( pred[0]=="grave"):
+                                result="Grave"
+                               
+                        else:
+                                result="Moderado"
+                                
         else:
-                result="Ausência"
-        people = [{'prediction': result}]
-        
+                result="invalido"
+        people = [{'prediction': result}]  
         return jsonify(people[0]) 
 
 
---
+++ bckp.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
+import joblib
+import numpy as np
+import os
+#dependences from load image 
+from keras.preprocessing import image
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import preprocess_input
+#import cv2
+import json
+from flask_cors import CORS
+from PIL import Image
+from io import BytesIO
+import base64
+ 
+
+APP = Flask(__name__)
+API = Api(APP)
+app = Flask("_name_")
+CORS(app)
+
+@app.route('/ret', methods = ['GET'])
+def returnascii():
+        inputchr = str(request.args['nome'])
+        stringfinal=inputchr.replace(" ","+")
+        stringfinal=stringfinal.replace("data:image/png;base64,","")
+        strinfinal2=str.encode(stringfinal)
+        data={}
+        data['img'] = strinfinal2
+        im = Image.open(BytesIO(base64.b64decode(data['img'])))
+        image_result = open('deer_decode.png', 'wb') # create a writable image and write the decoding result
+        image_result.write(base64.b64decode(data['img']))
+      #  print(type(inputchr))
+        model = VGG19(weights='imagenet', include_top=False)
+        img = image.load_img('deer_decode.png', target_size=(224, 224))
+        img_data = image.img_to_array(img)
+        img_data = np.expand_dims(img_data, axis=0)
+        img_data = preprocess_input(img_data)
+        vgg19_feature_listteste=[]
+        vgg19_feature = model.predict(img_data)
+        vgg19_feature_np = np.array(vgg19_feature)
+        vgg19_feature_listteste.append(vgg19_feature_np.flatten())
+        crc =joblib.load('SVM_multiclasse.mdl')
+        pred= crc.predict(vgg19_feature_listteste)
+      
+        print(pred)
+        result = ""
+        
+        if( pred[0]=="leve"):
+                result="Leve"
+        if( pred[0]=="moderado"):
+                result="Moderado"
+        if( pred[0]=="grave"):
+                result="Grave"
+        else:
+                result="Ausência"
+        people = [{'prediction': result}]
+        
+        return jsonify(people[0]) 
+       
+       
+    
+
+app.run()
